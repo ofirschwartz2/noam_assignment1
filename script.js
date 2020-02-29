106ef99e478a6a764588b3232168ea7a65e2bb08
Ofir Schwartz <ofirschwartz2@gmail.com>
2020-02-29 10:23:32 +0200

    added winning/losing

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 //document.write("Basketball Game!");
 var timeLeft = 4;
+var ballID;
+var score = false;
 setInterval(decreaseTimer, 1000);
+
+function checkResult() {
+    if (score) {
+        window.alert("WIN");
+    } else {
+        window.alert("LOSE");
+    }
+    score = false;
+    document.getElementById("ball").append(document.getElementById(ballID));
+}
 
 function decreaseTimer() {
     if (timeLeft == 0) {
+        checkResult();
         timeLeft = 4;
     } else {
         timeLeft--;
     }
     document.getElementById("timer").innerHTML = timeLeft;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function dragStart(ev) {
+    ballID = ev.target.id;
+}
+
+function drop(ev) {
+    ev.target.append(document.getElementById(ballID));
+    score = true;
 }
